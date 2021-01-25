9ba62c341503503b57d091f8e762e06e0b2be209
quartz55 <john.razor97@gmail.com>
2021-01-25 17:08:46 +0000

    Updated and pinned AWS SSM plugin

--- darwin/pkgs/ssm.nix
+++ darwin/pkgs/ssm.nix
@@ -2,20 +2,19 @@
 
 stdenv.mkDerivation rec {
   pname = "ssm-session-manager-plugin";
-  version = "latest";
+  version = "1.2.30.0";
 
   src = fetchurl {
-    url = "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip";
-    sha256 = "1yvb3wgkbkp9xgf51vkkvmj6ngqq0zas8cg5yp8prryjv2jaizqw";
+    url = "https://s3.amazonaws.com/session-manager-downloads/plugin/${version}/mac/sessionmanager-bundle.zip";
+    sha256 = "sha256-kA0sOxBE7fcMsCQojVgbLMz1zU1e0wYeUptUm2ewvAU=";
   };
 
   phases = [ "installPhase" ];
 
-  buildInputs = with pkgs; [ awscli unzip ];
+  buildInputs = with pkgs; [ unzip ];
 
   installPhase =
     ''
-    echo $(pwd)
     unzip ${src}
     mkdir -p $out/bin
     mv sessionmanager-bundle/bin/session-manager-plugin $out/bin/session-manager-plugin
